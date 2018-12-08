58d3a0c3c7b69e9bcb75cd58393b5d6c28733425
roycrippen <roycrippen@gmail.com>
2018-12-08 14:00:42 -0600

    day 2 part b

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,9 +7,8 @@ fn main() {
         .map(|elt| elt.to_string())
         .collect();
 
-    let test_data = get_test_data();
     println!("part a: {}", part_a(&xs));
-    println!("part b: {}", part_b(&test_data));
+    println!("part b: {}", part_b(&xs));
 }
 
 fn part_a(xs: &Vec<String>) -> usize {
@@ -19,8 +18,37 @@ fn part_a(xs: &Vec<String>) -> usize {
     two * three
 }
 
-fn part_b(_xs: &Vec<String>) -> i32 {
-    unimplemented!()
+fn part_b(xs: &Vec<String>) -> String {
+    for x in xs {
+        for y in xs {
+            match diff_by_one_char(&x, &y) {
+                Some(v) => return v,
+                None => continue,
+            }
+        }
+    }
+    "NO MATCH".to_string()
+}
+
+fn diff_by_one_char(s1: &String, s2: &String) -> Option<String> {
+    if s1.len() != s2.len() {
+        return None;
+    }
+
+    let cs1: Vec<char> = s1.chars().collect();
+    let cs2: Vec<char> = s2.chars().collect();
+
+    let res_str: String = cs1
+        .iter()
+        .zip(cs2.iter())
+        .filter(|(e1, e2)| e1 == e2)
+        .map(|(e1, _)| *e1)
+        .collect();
+
+    match s1.len() - res_str.len() {
+        1 => Some(res_str),
+        _ => None,
+    }
 }
 
 fn occurences(s: &String) -> (bool, bool) {
@@ -37,39 +65,44 @@ fn occurences(s: &String) -> (bool, bool) {
     (xs.contains(&2), xs.contains(&3))
 }
 
-fn get_test_data() -> Vec<String> {
-    vec![
-        "abcdef".to_string(),
-        "bababc".to_string(),
-        "abbcde".to_string(),
-        "abcccd".to_string(),
-        "aabcdd".to_string(),
-        "abcdee".to_string(),
-        "ababab".to_string(),
-    ]
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_occurances() {
-    let xs: Vec<(bool, bool)> = get_test_data().iter().map(occurences).collect();
-    let res = [
-        (false, false),
-        (true, true),
-        (true, false),
-        (false, true),
-        (true, false),
-        (true, false),
-        (false, true),
-    ];
-    assert_eq!(xs, res);
-}
+    fn get_test_data() -> Vec<String> {
+        vec![
+            "abcdef".to_string(),
+            "bababc".to_string(),
+            "abbcde".to_string(),
+            "abcccd".to_string(),
+            "aabcdd".to_string(),
+            "abcdee".to_string(),
+            "ababab".to_string(),
+        ]
+    }
 
-#[test]
-fn test_part_a() {
-    assert_eq!(part_a(&get_test_data()), 12);
-}
+    #[test]
+    fn test_occurances() {
+        let xs: Vec<(bool, bool)> = get_test_data().iter().map(occurences).collect();
+        let res = [
+            (false, false),
+            (true, true),
+            (true, false),
+            (false, true),
+            (true, false),
+            (true, false),
+            (false, true),
+        ];
+        assert_eq!(xs, res);
+    }
 
-#[test]
-fn test_part_b() {
-    assert_eq!(true, true);
+    #[test]
+    fn test_part_a() {
+        assert_eq!(part_a(&get_test_data()), 12);
+    }
+
+    #[test]
+    fn test_part_b() {
+        assert_eq!(part_b(&get_test_data()), "abcde".to_string());
+    }
 }
